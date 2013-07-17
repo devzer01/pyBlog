32d4d2d94580c3230580acc649a98fa65200f41d
devzero <nayana@corp-gems.com>
2013-07-17 18:00:27 +0700

    commit

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,18 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from posts.models import Post
 
 def index(request):
-	return HttpResponse("Welcome to pyBlog")
+	posts = Post.objects.order_by('-pub_date')[:5]
+	#output = ', '.join([p.title for p in posts])
+	#return HttpResponse(output)
+	template =loader.get_template('posts/index.html')
+	context = RequestContext(request, {
+		'posts' : posts,
+	})
+	return HttpResponse(template.render(context))
+
+
